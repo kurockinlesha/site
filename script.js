012fe0015e1b22bd8526058db1f6cc663f801f86
Kurochkin Alexey <kurockinaleksej29@gmail.com>
2023-05-13 08:48:22 +0300

    внесены исправления

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let a = 10;
+a = 20;
 alert(a);
-alert(a+10);
-let iphone = 2007;
+const iphone = 2007;
 alert(iphone);
-let creator = 'Брендан';
+const creator = 'Брендан';
 alert(creator);
 let b = 2;
-alert(a+b);
-alert(a-b);
-alert(a*b);
-alert(a/b);
+let d = 10;
+alert(d+b);
+alert(d-b);
+alert(d*b);
+alert(d/b);
 let result = b**5;
 alert(result);
 let c = 9;
@@ -19,19 +20,19 @@ num += 5;
 num -= 3;
 num *= 7;
 num /= 3;
-num += 1;
-num -= 1;
+num ++;
+num --;
 alert(num);
 let age = Number(prompt('Сколько вам лет?'));
 alert(`Вам ${age}`);
 const user = {
-    name: String('Дмитрий'),
-    age: Number('28'),
-    isAdmin: Boolean('Админ'),
+    name: 'Дмитрий',
+    age: 28,
+    isAdmin: false,
     "city of residence": true,
-    age: Number('25'),
-    delete: "city of residence"
 };
+user.age = 23;
+delete user["city of residence"];
 let info = prompt("Какую информацию хотите узнать о пользователе?", "name");
 alert(user[info]);
 let username = String(prompt('Как вас зовут?'));
